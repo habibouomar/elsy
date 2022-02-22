7bf3d6b0d0dd2a3f0fa4f677c8fb744fb8796fba
habibouomar <habibou.omar@gmail.com>
2022-02-22 16:43:35 +0100

    first commit part I

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box } from "./components/Box";
+import './styles/global.css';
+
+const tempMin = -20;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180
+const stepsMin = 0;
+const stepsMax = 50000;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <div class="container-fluid">
+        <div class="row">
 
+        <Box icon="directions_walk" color="black" value= "3000" unit="Steps"/>
+        <Box icon="local_drink" color="#3A85FF" value= "1.5" unit="L"/>
+        <Box icon="favorite" color="red" value= "120" unit="bpm"/>
+        <Box icon="wb_sunny" color="yellow" value= "-10" unit="°C"/>
+       
+      
+        <p>Heart : {heartMin}</p>
+        <p>Tempearture : {tempMin}</p>
+        <p>Steps : {stepsMin}</p>
+
+        </div>
+      </div>
+    );
+}
 export default App;
